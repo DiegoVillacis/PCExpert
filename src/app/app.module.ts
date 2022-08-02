9a2cc08e909b7c35211f656c7669a213a39efabe
DiegoVillacis <95387849+DiegoVillacis@users.noreply.github.com>
2022-08-02 12:35:48 -0500

    PAGES carpeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,16 @@
-import { Component, NgModule } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Observable } from 'rxjs';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SidenavComponent
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
-    Component,
-    FormControl,
-    Observable,
-    HomeComponent,
-    AppComponent,
-    
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
