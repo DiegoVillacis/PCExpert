e1ed539e7a08569a4f1bb743ea9d91b96ce5c0e7
DiegoVillacis <95387849+DiegoVillacis@users.noreply.github.com>
2022-08-02 12:15:48 -0500

    Creacion del proyecto y 2 componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidenavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Component,
+    FormControl,
+    Observable,
+    HomeComponent,
+    AppComponent,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
