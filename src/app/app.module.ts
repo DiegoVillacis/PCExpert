4154dd89078f419a16c8363c043a16344a193968
JenniferSalinas16 <jsalinas3560@uta.edu.ec>
2022-08-02 12:56:29 -0500

    commit detalle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { HomeComponent } from './pages/home/home.component';
 import { IndexComponent } from './pages/index/index.component';
+import { DetalleComponent } from './page/detalle/detalle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
     HomeComponent,
-    IndexComponent
+    IndexComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule
