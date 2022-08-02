b22388f2603dda77b3248c24350c3a19d9e2d1a4
DiegoVillacis <95387849+DiegoVillacis@users.noreply.github.com>
2022-08-02 12:48:20 -0500

    Component HOME

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    HomeComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule
